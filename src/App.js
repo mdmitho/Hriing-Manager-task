e7f0d18f19cdd322b3eff2b40cc5ab69c24f050d
Awlad Hossen <mdawladhossenmito199922@gmail.com>
2022-11-24 14:21:12 +0600

    daisyUI themes Add done

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <h1 className="text-3xl font-bold underline mt-5">
-      Hello world!
+       <h1 className="text-3xl font-bold  mt-5">
+      Hiring Manager Task
     </h1>
+      
+      
+
+
     </div>
   );
 }
